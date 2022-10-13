413a7f927dda0c235eb4bc5ae88f5070da69dfa5
stove <stove@rivosinc.com>
2022-10-13 15:27:02 -0700

    RIVOS: add papi to overlay in flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,42 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
 
-  outputs = { self, nixpkgs }: rec {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    version = "6.0.0.1-g${self.rev or "dirty"}";
+    # System types to support.
+    supportedSystems = [
+      "riscv64-linux"
+      "x86_64-linux"
+    ];
 
-    packages.x86_64-linux.papi = nixpkgs.legacyPackages.x86_64-linux.papi.overrideAttrs (oldAttrs: { src = self; } );
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.papi;
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
 
+        overlays = [
+          self.overlays.default
+        ];
+      });
+  in rec {
+    overlays.default = final: prev: {
+      papi = prev.papi.overrideAttrs (oldAttrs: {
+        inherit version;
+        src = self;
+      });
+    };
+
+    packages = forAllSystems (system: let
+      papi = (nixpkgsFor.${system}).papi;
+    in {
+      inherit papi;
+      default = papi;
+    });
   };
 }
